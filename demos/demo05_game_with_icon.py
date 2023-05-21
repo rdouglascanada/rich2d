df6a64a1dd770ea16d948924ffcd679d60073119
rdouglascanada <rdouglascanada@gmail.com>
2023-05-21 16:58:52 -0400

    0.6.2 Better value for Demo Icon

--- demos/demo05_game_with_icon.py
+++ demos/demo05_game_with_icon.py
@@ -1,5 +1,5 @@
 from game import Game, GameConfig
 
-icon_config = GameConfig(window_icon="../resources/demo05/example_icon.jpg")
+icon_config = GameConfig(window_icon="../resources/demo05/checkmark_icon.ico")
 icon_game = Game(config=icon_config)
 icon_game.start()
